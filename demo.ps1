a8be7785b7449b7bf3d69e6794ce74b4a6b14aed
Travis Plunk <travis.plunk@microsoft.com>
2015-06-03 14:05:54 -0700

    use ISE

--- demo.ps1
+++ demo.ps1
@@ -6,11 +6,11 @@ Get-ChildItem .\DscResources
 # Let's start with the composite resource
 # It is composed of the 4 DSC xMySql Script Resources and
 # a package resource.
-Get-Content .\DscResources\xMySqlProvision\xMySqlProvision.Schema.psm1
+powershell_ise.exe .\DscResources\xMySqlProvision\xMySqlProvision.Schema.psm1
 
 # Let's move on to an example of how to use the 
 # Composite resource.
-Get-Content .\Examples\Sample_MySQL_Provision.ps1
+powershell_ise.exe .\Examples\Sample_MySQL_Provision.ps1
 
 # Get the existing VM for the demo
 $vm = Get-AzureDemoVm 
